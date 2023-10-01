6205da02409c6778d048fd579589599f1043a478
LucasVieira <lucasvieirasants@gmail.com>
2023-10-01 16:05:09 -0300

    estudo Métodos

--- src/contas/Conta.java
+++ src/contas/Conta.java
@@ -11,4 +11,21 @@ public class Conta {
     protected void exibirSaldo(){
         System.out.println("Saldo:  R$ " + saldo);
     }
+    void sacar(double valor){
+        saldo -= valor;
+        System.out.println("Débito: R$ " + valor);
+    }
+    void depositar(double valor){
+        saldo += valor;
+        System.out.println("Crédito: R$ " + valor);
+    }
+    void transferir(Conta destino, double valor){
+        this.sacar(valor);
+        destino.depositar(valor);
+        System.out.println("Tranferência: " + valor);
+    }
+    double soma(double cc1, double cc2){
+        double total = cc1 + cc2;
+        return total;
+    }
 }
--- src/contas/PessoaFisica.java
+++ src/contas/PessoaFisica.java
@@ -7,11 +7,31 @@ public class PessoaFisica {
         cc1.saldo = 10000;
         System.out.println("Cliente: " + cc1.cliente);
         cc1.exibirSaldo();
+        cc1.sacar(1000);
+        cc1.exibirSaldo();
         System.out.println("_______________________________");
         Conta cc2 = new Conta();
         cc2.cliente = "Sirlene Sanches";
         cc2.saldo = 8500;
         System.out.println("Cliente: " + cc2.cliente);
         cc2.exibirSaldo();
+        cc2.depositar(5000);
+        cc2.exibirSaldo();
+        System.out.println("_______________________________");
+        System.out.println("Transferência");
+        System.out.println("Cliente: " + cc1.cliente);
+        System.out.println("Favorecido: " + cc2.cliente);
+        cc1.transferir(cc2, 2000);
+        System.out.println("");
+        System.out.println("Cliente: " + cc1.cliente);
+        cc1.exibirSaldo();
+        System.out.println("Cliente: " + cc2.cliente);
+        cc2.exibirSaldo();
+        System.out.println("_______________________________");
+        System.out.println("Relatório Gerencial");
+        Conta gerente = new Conta();
+        double relatorio = gerente.soma(cc1.saldo, cc2.saldo);
+        System.out.println("Saldo total nas contas: R$ " + relatorio);
+
     }
 }
